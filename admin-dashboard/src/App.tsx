084dd7d8dc9a88276c80b937ead95f56aa342741
vatsaljain-dev <vatsaljain.dev@gmail.com>
2025-09-01 00:51:32 +0530

    admin-dashboard-fixes

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
